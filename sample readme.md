98b2fd4fb06b4b223efd0577f1d63b516a02169a
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-07-25 16:57:54 -0400

    finish liecnse section; create sample readme.md

---
+++ sample readme.md
@@ -0,0 +1,38 @@
+# Simplify Project Documentation - ReadMe.MD Generator
+
+  
+[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)  
+
+## Description
+Focus your efforts on what you do best and let AWOLRob's readme.md generator handle the task.  Generate quality readme files for all your projects.
+
+## Table of contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#test)
+- [Questions](#questions)
+  
+## Installation
+- First 'npm init', then run 'npm install inquirer' finally from the command line type 'node index.js'
+
+## Usage
+Use my readme generator for all your projects.  After installing the required modules, simply type node index.js from a command line within your project folder
+
+## License
+
+Licensed under the [IBM](https://opensource.org/licenses/IPL-1.0) Public License Version 1.0 
+
+## Contributing
+There are many ways in which you can participate in this project, for example:
+
+- I'm open to any and all contributions.  Reach out to me under the Questions section below.
+
+## Test
+- The easiest way to test is to accept all the default values when prompted.  Then open the README.MD file created in the project directory
+
+## Questions
+* Please feel free to leave comments on my GitHub profile [GitHub](https://github.com/awolrob)
+* Or email me with your questions at ( [awolrob@gmail.com](mailto:awolrob@gmail.com) )
+ 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,62 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
 
-}
+  switch (license) {
+    case "MIT":
+      return `
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`;
+    case "IBM":
+      return `
+[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)]`;
+    case "Unlicense":
+      return `
+[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]`;
+    default: return license
+  };
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
 
-}
+  switch (license) {
+    case "MIT":
+      return `(https://opensource.org/licenses/MIT)  `;
+    case "IBM":
+      return `(https://opensource.org/licenses/IPL-1.0)  `;
+    case "Unlicense":
+      return `(http://unlicense.org/)  `;
+    default: return license
+  };
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-}
+  if (!license) {
+    return '';
+  }
+
+  switch (license) {
+    case "MIT":
+      return `
+Licensed under the [MIT](https://opensource.org/licenses/MIT) license. `;
+    case "IBM":
+      return `
+Licensed under the [IBM](https://opensource.org/licenses/IPL-1.0) Public License Version 1.0 `;
+    case "Unlicense":
+      return `
+Licensed under The [Unlicense](http://unlicense.org/) `;
+    default: return license
+  };
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -21,9 +64,8 @@ function generateMarkdown(data) {
   // return `# ${data.projectTitle}
   return `# ${data.projectTitle}
 
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)
-[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+  ${renderLicenseBadge(data.projectLicense)}${renderLicenseLink(data.projectLicense)}
+
 ## Description
 ${data.projectDesc}
 
@@ -32,32 +74,25 @@ ${data.projectDesc}
 - [Usage](#usage)
 - [License](#license)
 - [Contributing](#contributing)
-- [Tests](#tests)
+- [Tests](#test)
 - [Questions](#questions)
   
 ## Installation
-${data.projectDesc}
+- ${data.projectInstall}
 
 ## Usage
 ${data.projectUsage}
 
 ## License
-${data.projectLicense}
-
-Licensed under the [MIT](https://opensource.org/licenses/MIT) license.
-
-Licensed under the [IBM](https://opensource.org/licenses/IPL-1.0) Public License Version 1.0 
-
-Licensed under The [Unlicense](http://unlicense.org/)
+${renderLicenseSection(data.projectLicense)}
 
 ## Contributing
-${data.projectContrib}
-
 There are many ways in which you can participate in this project, for example:
 
-## Test
-${data.projectTest}
+- ${data.projectContrib}
 
+## Test
+- ${data.projectTest}
 
 ## Questions
 * Please feel free to leave comments on my GitHub profile [GitHub](${data.github})
